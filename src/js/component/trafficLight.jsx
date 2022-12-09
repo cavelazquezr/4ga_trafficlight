f307bd8caa5484f72f1f3d86edd82a32a8861e4f
Carlos Alberto <95.carlos.velazquez@gmail.com>
2022-12-09 20:18:34 +0000

    first bonus added

--- src/js/component/trafficLight.jsx
+++ src/js/component/trafficLight.jsx
@@ -9,9 +9,7 @@ const TrafficLight = () => {
 	let [colorYellow, setSelectedYellow] = useState("");
 	let [colorGreen, setSelectedGreen] = useState("");
 
-	const changeColor=(event)=>{
-		let aux = event.target.id;
-		
+	const glowRender = (aux) => {
 		if(aux==="red"){
 			setSelectedRed("selected");
 			setSelectedYellow("");
@@ -28,16 +26,33 @@ const TrafficLight = () => {
 			setSelectedGreen("selected");
 		}
 	}
+
+	const changeColor=(event)=>{
+		let aux = event.target.id;
+		glowRender(aux);
+	}
+
+	const randomColor = () => {
+		const trafficLights = ["red", "yellow", "green"];
+
+		let randomColor = trafficLights[Math.floor(Math.random()*3)];
+
+		console.log(randomColor)
+
+		glowRender(randomColor)
+	};
 	
 	return (
-		<>
+		<div className="d-flex justify-content-center flex-column">
 		<div className="trafficTop"></div>
 		<div className="container">
 			<div onClick={changeColor} id="red" className={`red light ${colorRed}`}></div>
 			<div onClick={changeColor} id="yellow" className={`yellow light ${colorYellow}`}></div>
 			<div onClick={changeColor} id="green" className={`green light ${colorGreen}`}></div>
 		</div>
-		</>
+
+		<button onClick={randomColor} type="button" className="btn btn-dark w-25 m-auto mt-2">Random</button>
+		</div>
 	);
 };
 
